09ff68300b4f50875e02738609424e8e4abe2724
Didier Ungar <didier.ungar@gmail.com>
2020-04-10 01:43:05 +0200

    ajoute de la methode getUrl dans Job + code style

--- src/Jenkins/Job.php
+++ src/Jenkins/Job.php
@@ -8,14 +8,13 @@ class Job
 {
 
     /**
-     * @var \stdClass
-     */
-    private $job;
-
-    /**
      * @var Jenkins
      */
     protected $jenkins;
+    /**
+     * @var \stdClass
+     */
+    private $job;
 
     /**
      * @param \stdClass $job
@@ -33,7 +32,7 @@ class Job
      */
     public function getBuilds()
     {
-        $builds = array();
+        $builds = [];
         foreach ($this->job->builds as $build) {
             $builds[] = $this->getJenkinsBuild($build->number);
         }
@@ -54,78 +53,6 @@ class Job
     }
 
     /**
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->job->name;
-    }
-
-    /**
-     * @return array
-     */
-    public function getParametersDefinition()
-    {
-        $parameters = array();
-
-        foreach ($this->job->actions as $action) {
-            if (!property_exists($action, 'parameterDefinitions')) {
-                continue;
-            }
-
-            foreach ($action->parameterDefinitions as $parameterDefinition) {
-                $default     = property_exists($parameterDefinition, 'defaultParameterValue')
-                               && isset($parameterDefinition->defaultParameterValue->value)
-                    ? $parameterDefinition->defaultParameterValue->value
-                    : null;
-                $description = property_exists($parameterDefinition, 'description')
-                    ? $parameterDefinition->description
-                    : null;
-                $choices     = property_exists($parameterDefinition, 'choices')
-                    ? $parameterDefinition->choices
-                    : null;
-
-                $parameters[$parameterDefinition->name] = array(
-                    'default'     => $default,
-                    'choices'     => $choices,
-                    'description' => $description,
-                );
-            }
-        }
-
-        return $parameters;
-    }
-
-    /**
-     * @return string
-     */
-    public function getColor()
-    {
-        return $this->job->color;
-    }
-
-    /**
-     * @return string
-     *
-     * @throws \RuntimeException
-     */
-    public function retrieveXmlConfigAsString()
-    {
-        return $this->jenkins->retrieveXmlConfigAsString($this->getName());
-    }
-
-    /**
-     * @return \DOMDocument
-     */
-    public function retrieveXmlConfigAsDomDocument()
-    {
-        $document = new \DOMDocument;
-        $document->loadXML($this->retrieveXmlConfigAsString());
-
-        return $document;
-    }
-
-    /**
      * @return Jenkins
      */
     public function getJenkins()
@@ -143,6 +70,83 @@ class Job
         $this->jenkins = $jenkins;
 
         return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->job->name;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->job->url;
+    }
+
+    /**
+     * @return array
+     */
+    public function getParametersDefinition()
+    {
+        $parameters = [];
+
+        foreach ($this->job->actions as $action) {
+            if (!property_exists($action, 'parameterDefinitions')) {
+                continue;
+            }
+
+            foreach ($action->parameterDefinitions as $parameterDefinition) {
+                $default = property_exists($parameterDefinition, 'defaultParameterValue')
+                && isset($parameterDefinition->defaultParameterValue->value)
+                    ? $parameterDefinition->defaultParameterValue->value
+                    : null;
+                $description = property_exists($parameterDefinition, 'description')
+                    ? $parameterDefinition->description
+                    : null;
+                $choices = property_exists($parameterDefinition, 'choices')
+                    ? $parameterDefinition->choices
+                    : null;
+
+                $parameters[ $parameterDefinition->name ] = [
+                    'default'     => $default,
+                    'choices'     => $choices,
+                    'description' => $description,
+                ];
+            }
+        }
+
+        return $parameters;
+    }
+
+    /**
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->job->color;
+    }
+
+    /**
+     * @return \DOMDocument
+     */
+    public function retrieveXmlConfigAsDomDocument()
+    {
+        $document = new \DOMDocument;
+        $document->loadXML($this->retrieveXmlConfigAsString());
+
+        return $document;
+    }
+
+    /**
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    public function retrieveXmlConfigAsString()
+    {
+        return $this->jenkins->retrieveXmlConfigAsString($this->getName());
     }
 
     /**
